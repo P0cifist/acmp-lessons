94f24209a661b56faa1f635334bcc32b1856eb27
P0cifist <20serega06@gmail.com>
2025-02-20 19:25:52 +0300

    Create Task008.java

---
+++ src/Task008.java
@@ -0,0 +1,20 @@
+public class Task008 {
+    public static void main(String[] args) {
+        int hundreds;
+        int tens;
+        int units;
+        for (int i = 100; i <= 999; i++) {
+            do{
+                hundreds = i / 100;
+                tens = i / 10  % 10;
+                units = i % 100;
+                if (hundreds != tens && hundreds != units && tens != units) {
+                    System.out.println(i);
+                }
+                hundreds = 0;
+                tens = 0;
+                units = 0;
+            } while (hundreds != 0 && tens != 0 && units != 0);
+        }
+    }
+}
