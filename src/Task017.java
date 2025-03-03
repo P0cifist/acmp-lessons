436d42a4d11cbfa5423758e5bcc138f4693086f5
P0cifist <20serega06@gmail.com>
2025-03-04 00:59:36 +0300

    Create Task017.java

---
+++ src/Task017.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task017 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Enter no. of elements in array: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        Arrays.sort(numbers);
+
+        int pairCount = 0;
+        int currentCount = 1;
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = rand.nextInt(10);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] == numbers[i - 1]) {
+                currentCount++;
+            } else {
+                if (currentCount >= 2) {
+                    pairCount += currentCount * (currentCount - 1) / 2;
+                }
+                currentCount = 1;
+            }
+        }
+
+        if (currentCount >= 2) {
+            pairCount += currentCount * (currentCount - 1) / 2;
+        }
+
+        System.out.println(pairCount);
+
+    }
+}
